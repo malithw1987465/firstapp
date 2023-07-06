2ba6a5737c91454deaa8e87c847f4e32d27f8a76
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-06 20:59:53 +0530

    Just a navbar

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,8 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+const Style={
+  color:"#282c34",
+}
 function Header(){
-  return <h1>Hello World !</h1>;
+  return(
+    <div className="home" style={Style}>
+        <nav>
+          <ul>
+            <li >Home</li>
+            <li>About</li>
+            <li>Quiz</li>
+          </ul>
+        </nav>
+    </div>
+  );
 }
 function App() {
  return <Header/>;
