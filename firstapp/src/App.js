bef4ed31b19d7c01ab12975db27d72835ed96770
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-08 23:44:43 +0530

    Mnaging the state in react

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,55 +1,21 @@
+import React from "react";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
-function Main(props) { 
-  return <Header msg={props.msg} />; 
-};
+function App() {
+  const [fruits] = React.useState([
+      {fruitName: 'apple', id: 1},
+      {fruitName: 'apple', id: 2},
+      {fruitName: 'plum', id: 3},
+  ]);
 
-function Header(props) { 
-  return ( 
-    <div style={{ border: "10px solid whitesmoke" }}> 
-      <h1>Header here</h1> 
-      <Wrapper msg={props.msg} /> 
-    </div> 
-  ); 
-};
-
-function Wrapper(props) { 
-  return ( 
-    <div style={{ border: "10px solid lightgray" }}> 
-      <h2>Wrapper here</h2> 
-      <Button msg={props.msg} /> 
-    </div> 
-  ); 
-};
-
-function Button(props) { 
-  return ( 
-    <div style={{ border: "20px solid orange" }}> 
-      <h3>This is the Button component</h3> 
-      <button onClick={() => alert(props.msg)}>Click me!</button> 
-    </div> 
-  ); 
-};
-
-function App() { 
-  return ( 
-    <Main  
-      msg="I passed through the Header and the Wrapper and I reached the Button component"  
-    /> 
-  ); 
-}; 
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits} />
+    </div>
+  );
+}
 
 export default App;
-
-// This app is simple enough that you should be able to understand it on your own. Let’s address the main points to highlight what is happening in the code above.
-
-// The top-most component of this app is the App component. The App component returns the Main component. The Main component accepts a single attribute, named msg, as in “message”.
-
-// At the very top of the app, the Main function declares how the Main component should behave. The Main component is responsible for rendering the Header component. Note that when the Header component is rendered from inside Main, it also receives the msg prop.
-
-// The Header component’s function declaration renders an h1 that reads “Header here”, then another component named Wrapper. Note that the naming here is irrelevant – the components Header and Wrapper are named to make it a bit more like it might appear in a real app – but ultimately, the focus is on having multiple components, rather than describing specific component names properly.
-
-// So, the Header component’s function declaration has a return statement, which renders the Wrapper component with the msg prop passed to it.
-
-// In the Wrapper component’s function declaration, there’s an h2 that reads “Wrapper here”, in addition to the rendering of the Button component, which also receives the msg attribute.
-
-// Finally, the Button component’s function declaration is coded to receive the props object, then inside of the wrapping div, show an h3. The h3 reads “This is the Button component”, and then, under that, there’s a button element with an onClick event-handling attribute. This is passed to an arrow function which should alert the string that comes from the props.msg prop.
