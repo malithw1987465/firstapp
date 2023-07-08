a279cd37001b792aea8f920bbcf5601a4bb49053
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-08 20:12:17 +0530

    Observing state

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Heading from './Heading';
-import '/App.css';
+
 
 function App() {
   const [word,setWord] = React.useState('Eat')
@@ -9,10 +9,10 @@ function App() {
     setWord('Drink')
 
   }
- 
+  
   return(
     <div className="App">
-      <Heading message={word+"at Little Lemon"}/>
+      <Heading message={word +" at Little Lemon"}/>
       <button onClick={handleClick}>Click Here!</button>
     </div>
   );
