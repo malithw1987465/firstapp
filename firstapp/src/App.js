6095f665ceb54bd22df3e6a4eba57dc289f53584
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-08 20:42:44 +0530

    Updated Incomplted Meals APP Described in video

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,14 @@
-import React, {useState} from "react";
-import Heading from './Heading';
-
+import Counter from './Counter';
+import MealsList from './MealsList';
 
 function App() {
-  const [word,setWord] = React.useState('Eat')
-
-  function handleClick(){
-    setWord('Drink')
-
-  }
   
   return(
-    <div className="App">
-      <Heading message={word +" at Little Lemon"}/>
-      <button onClick={handleClick}>Click Here!</button>
+    <div>
+    <Counter/>
+    <MealsList/>
     </div>
+   
   );
 }
 
---
+++ firstapp/src/MealsList.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function MealsList(){
+    return(
+        const MealsList= ( )=>{
+            const todayMeals=[
+                {name:"Baked Beans",calories:200,id:12345},
+                {name:"Grilled Veggies",calories:80,id:23456},
+                {name:"Soup",calories:100,id:34567}
+            ]
+            const [meals, setMeals]=useState(todayMeals);
+        }
+    );
+}
