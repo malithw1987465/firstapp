56ccedbc4e8136179effd79591bbc6f78470e671
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-08 20:49:43 +0530

    Clarification of the props drilling in the app

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -39,3 +39,17 @@ function App() {
 }; 
 
 export default App;
+
+// This app is simple enough that you should be able to understand it on your own. Let’s address the main points to highlight what is happening in the code above.
+
+// The top-most component of this app is the App component. The App component returns the Main component. The Main component accepts a single attribute, named msg, as in “message”.
+
+// At the very top of the app, the Main function declares how the Main component should behave. The Main component is responsible for rendering the Header component. Note that when the Header component is rendered from inside Main, it also receives the msg prop.
+
+// The Header component’s function declaration renders an h1 that reads “Header here”, then another component named Wrapper. Note that the naming here is irrelevant – the components Header and Wrapper are named to make it a bit more like it might appear in a real app – but ultimately, the focus is on having multiple components, rather than describing specific component names properly.
+
+// So, the Header component’s function declaration has a return statement, which renders the Wrapper component with the msg prop passed to it.
+
+// In the Wrapper component’s function declaration, there’s an h2 that reads “Wrapper here”, in addition to the rendering of the Button component, which also receives the msg attribute.
+
+// Finally, the Button component’s function declaration is coded to receive the props object, then inside of the wrapping div, show an h3. The h3 reads “This is the Button component”, and then, under that, there’s a button element with an onClick event-handling attribute. This is passed to an arrow function which should alert the string that comes from the props.msg prop.
