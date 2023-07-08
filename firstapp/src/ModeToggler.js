cdf202072acdebd6d89b3d4c914312269d4ad7b8
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-08 11:58:06 +0530

    Event Handling light mode dark mode

--- firstapp/src/ModeToggler.js
+++ firstapp/src/ModeToggler.js
@@ -1,10 +1,25 @@
 function ModeToggler(){
-    const darkModeOn=false;
+    let darkModeOn=false;
     const darkMode=<h1>Dark Mode is on</h1>
     const lightMode=<h1>Light Mode is on</h1>
+
+
+    function handleClick(){
+        darkModeOn=!darkModeOn;
+
+        if(darkModeOn==true){
+            console.log("Dark Mode is On")
+        }else{
+            console.log("Ligth Mode is On")
+        }
+        
+    }
 return(
     <div>
         {darkModeOn ? darkMode : lightMode}
+        <button onClick={handleClick}>
+        Click me    
+        </button>
     </div>
 );
 }
