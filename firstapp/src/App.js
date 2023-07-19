7f7170be933ce8a584c0c5574935d7111165231b
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-07-19 17:49:11 +0530

    Converting this repo for ACPT React

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
-import Fruits from "./Fruits";
-import FruitsCounter from "./FruitsCounter";
 
 function App() {
-  const [fruits] = React.useState([
-      {fruitName: 'apple', id: 1},
-      {fruitName: 'apple', id: 2},
-      {fruitName: 'plum', id: 3},
-  ]);
-
-  return (
-    <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits={fruits} />
-      <FruitsCounter fruits={fruits} />
-    </div>
+  return(
+    <div>
+    <h1>
+      Hellooo.. Im Malith..
+    </h1>
+    <button>Click Me</button>
+  </div>
   );
 }
 
